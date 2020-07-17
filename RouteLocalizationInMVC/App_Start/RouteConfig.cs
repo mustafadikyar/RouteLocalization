839c6b1771d6bd2b85b0bb6dcb41c06a84ec4c58
Mustafa Dikyar <msdikyar@hotmail.com>
2020-07-17 18:40:58 +0300

    Add project files.

---
+++ RouteLocalizationInMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,51 @@
+﻿using RouteLocalization.Mvc;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using RouteLocalization.Mvc.Setup;
+using RouteLocalizationInMVC.Controllers;
+
+namespace RouteLocalizationInMVC
+{
+    public class RouteConfig
+    {
+        const string DefaultCulture = "tr";
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes(Localization.LocalizationDirectRouteProvider);
+
+            ISet<string> acceptedCultures = new HashSet<string>() { "en", "fr", "tr" };
+
+            var configuration = new Configuration
+            {
+                DefaultCulture = DefaultCulture,
+                AcceptedCultures = acceptedCultures,
+                AttributeRouteProcessing = AttributeRouteProcessing.AddAsNeutralAndDefaultCultureRoute,
+                AddCultureAsRoutePrefix = true,
+                AddTranslationToSimiliarUrls = true,
+            };
+            var localization = new Localization(configuration);
+
+            localization.TranslateInitialAttributeRoutes();
+
+            localization.ForCulture("en")
+                .ForController<DefaultController>()
+                .ForAction(c => c.Index()).AddTranslation("mainpage")
+                .ForAction(c=> c.About()).AddTranslation("about")
+                .ForAction(c=> c.Contact()).AddTranslation("contact");
+
+            localization.ForCulture("fr")
+                .ForController<DefaultController>()
+                .ForAction(c => c.Index()).AddTranslation("pagedaccueil")
+                .ForAction(c => c.About()).AddTranslation("àpropos")
+                .ForAction(c => c.Contact()).AddTranslation("contact");
+
+
+            CultureSensitiveHttpModule.GetCultureFromHttpContextDelegate = Localization.DetectCultureFromBrowserUserLanguages(acceptedCultures, DefaultCulture);
+        }
+    }
+}
+
---
+++ RouteLocalizationInMVC/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace RouteLocalizationInMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Route("anasayfa")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [Route("hakkinda")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("iletisim")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
